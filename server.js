7a798496491b4380fc8a75ba8421ed4b0183d276
Benjamin <benjaminvanzwienen@live.nl>
2016-08-15 21:29:54 +0200

    Add the possibility to POST and GET data from a MongoDB database

--- server.js
+++ server.js
@@ -1,24 +1,54 @@
 var express = require("express");
-//var bodyParser = require('body-parser');
+var MongoClient = require("mongodb").MongoClient;
+var dotenv = require('dotenv').config({path: "main.env"});
 
 var app = express();
 var port = process.env.PORT || 8080;
-var values = [];
+//var url = "mongodb://bvanzwienen:arduinoFree@ds042379.mlab.com:42379/arduino";
+var url = process.env.DB_URL;
 
-//app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(bodyParser.json());
+app.get("/", function(req, res){
+  res.end("Try /getData to get data.");
+});
 
-app.get('/', function(req, res) {
-  res.end(values.join("\n"));
+app.get('/getData', function(req, res) {
+  getData(function(docs){
+    res.send(docs);
+    res.end("");
+  });
 });
 
 app.post('/data', function(req, res){
-	var value = req.query.value;
-	values.push(value);
-  console.log(values);
-	res.end(value);
+  var value = req.query.value;
+  saveData(value);
+	res.end("");
 });
 
 app.listen(port, function () {
   console.log('App listening on port', port);
 });
+
+
+function getData(callback){
+  MongoClient.connect(url, function(err, db){
+    var collection = db.collection("test");
+
+    collection.find({}).toArray(function(err, docs){
+      callback(docs);
+    });
+
+    db.close();
+  });
+}
+
+function saveData(val){
+  MongoClient.connect(url, function(err, db){
+    var collection = db.collection("test");
+
+    collection.insert({
+      value: val
+    });
+
+    db.close();
+  });
+}
